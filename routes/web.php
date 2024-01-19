7abd8193a138d3ab268eb1d0a1331384168b5a52
Crisitan Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-01-19 23:25:02 +0100

    Noves rutes web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\ActivitatController;
+use App\Http\Controllers\ContingutController;
+use App\Http\Controllers\CriteriController;
+use App\Http\Controllers\ModulController;
+use App\Http\Controllers\ProgramacionController;
+use App\Http\Controllers\RaController;
+use App\Http\Controllers\UfController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +33,11 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('/moduls', ModulController::class);
+Route::resource('/ufs', UfController::class);
+Route::resource('/ras', RaController::class);
+Route::resource('/criteris', CriteriController::class);
+Route::resource('/continguts', ContingutController::class);
+Route::resource('/programacions', ProgramacionController::class);
+Route::resource('/activitats', ActivitatController::class);
