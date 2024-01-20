ca54d6644f51eacfc2087d9ce52f0d49bf4256a2
Crisitan Manuel Alcobendas Beorlegui <cristian.alcobendas@insbaixcamp.cat>
2024-01-20 17:36:01 +0100

    Correció en la definició de la taula

--- database/migrations/2024_01_19_214834_create_programacions_table.php
+++ database/migrations/2024_01_19_214834_create_programacions_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('year');
             $table->text('description')->nullable();
             $table->foreignId('modul_id')->constrained();
-            $table->foreignId('user_id')->constrianed();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
